074a51207af4ad75628dec98161228ec6e115af0
Kristiyan Halachev <kristianhalachev0@gmail.com>
2026-01-28 16:57:42 +0200

    Deploy: 2026-01-28 16:57:42

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,9 @@
+/**
+ * Puppeteer Configuration
+ * Skip downloading Chrome - use system Chromium instead
+ */
+
+module.exports = {
+  // Skip downloading Chrome during npm install
+  skipDownload: true,
+};
